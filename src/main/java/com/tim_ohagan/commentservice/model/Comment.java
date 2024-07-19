3e5fd8072d32db57f720269a35d0591b1c32a52a
Timothy Ohagan <tmohagan@gmail.com>
2024-07-19 11:17:23 -0400

    serilaze id

--- src/main/java/com/tim_ohagan/commentservice/model/Comment.java
+++ src/main/java/com/tim_ohagan/commentservice/model/Comment.java
@@ -5,11 +5,14 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.Instant;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 @Data
 @Document(collection = "comments")
 public class Comment {
     @Id
+    @JsonSerialize(using = ToStringSerializer.class)
     private ObjectId id;
     private String postID;
     private String userID;
